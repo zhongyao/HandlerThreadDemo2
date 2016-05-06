8d2a82704312745a40dc9f7771435322d2630b84
hongri <1285052668@qq.com>
2016-05-06 18:14:34 +0800

    httpurlconnection

--- src/com/handlerthread/AsytaskActivity.java
+++ src/com/handlerthread/AsytaskActivity.java
@@ -1,12 +1,10 @@
 package com.handlerthread;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
 
 import android.app.Activity;
 import android.os.AsyncTask;
@@ -14,7 +12,7 @@ import android.os.Bundle;
 import android.util.Log;
 
 public class AsytaskActivity extends Activity {
-	private String url = "http://api.map.baidu.com/telematics/v3/weather?location=%E5%98%89%E5%85%B4&output=json&ak=5slgyqGDENN7Sy7pw29IUvrZ";
+//	public String url = "http://api.map.baidu.com/telematics/v3/weather?location=%E5%98%89%E5%85%B4&output=json&ak=5slgyqGDENN7Sy7pw29IUvrZ";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -29,20 +27,34 @@ public class AsytaskActivity extends Activity {
 		protected String doInBackground(URL... params) {
 			
 			String result = null;
-			try {
-				HttpGet get = new HttpGet(url);
-				HttpClient client = new DefaultHttpClient();
+					try {
+						URL url = new URL("http://api.map.baidu.com/telematics/v3/weather?location=%E5%98%89%E5%85%B4&output=json&ak=5slgyqGDENN7Sy7pw29IUvrZ");
+						
+						HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+						conn.setConnectTimeout(15*1000);
+						if (conn.getResponseCode() == 200) {
+							InputStream is = conn.getInputStream();
+//							result = 
+						}else {
+							
+						}
+						
+						
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+//				HttpGet get = new HttpGet(url);
+//				HttpClient client = new DefaultHttpClient();
+//				
+//				HttpResponse response = client.execute(get);
+//				
+//				if (response.getStatusLine().getStatusCode() == 200) {
+//					result = EntityUtils.toString(response.getEntity(),"utf-8");
+//					Log.d("yao", result);
+//				}
 				
-				HttpResponse response = client.execute(get);
+//				HttpURLConnection httpURLConnection = new 
 				
-				if (response.getStatusLine().getStatusCode() == 200) {
-					result = EntityUtils.toString(response.getEntity(),"utf-8");
-					Log.d("yao", result);
-				}
-				
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
 			
 			return result;
 		}
