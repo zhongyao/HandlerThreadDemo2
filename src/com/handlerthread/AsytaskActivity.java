5884eae2aadf04036ed6a9ccdd6aa97d3d6a0fa6
hongri <1285052668@qq.com>
2016-05-07 07:40:46 +0800

    AsyTaskActivity修改

--- src/com/handlerthread/AsytaskActivity.java
+++ src/com/handlerthread/AsytaskActivity.java
@@ -1,59 +1,97 @@
 package com.handlerthread;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.URL;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 
 public class AsytaskActivity extends Activity {
-	private String url = "http://api.map.baidu.com/telematics/v3/weather?location=%E5%98%89%E5%85%B4&output=json&ak=5slgyqGDENN7Sy7pw29IUvrZ";
+
+	private ImageView iv;
+	private long timecurrentTimeMillis, timecurrentTimeMillis2;
+	private long time;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_asynctask);
+
+		initView();
 
 		new MyTask().execute();
 	}
 
-	private class MyTask extends AsyncTask<URL, Void, String> {
+	private void initView() {
+		iv = (ImageView) findViewById(R.id.iv);
+	}
+
+	private class MyTask extends AsyncTask<URL, Void, Bitmap> {
 
 		@Override
-		protected String doInBackground(URL... params) {
-			
-			String result = null;
+		protected Bitmap doInBackground(URL... params) {
+
 			try {
-				HttpGet get = new HttpGet(url);
-				HttpClient client = new DefaultHttpClient();
-				
-				HttpResponse response = client.execute(get);
-				
-				if (response.getStatusLine().getStatusCode() == 200) {
-					result = EntityUtils.toString(response.getEntity(),"utf-8");
-					Log.d("yao", result);
+				timecurrentTimeMillis = System.currentTimeMillis();
+				URL url = new URL(
+						"http://g.hiphotos.baidu.com/image/pic/item/060828381f30e9244e3f894a49086e061d95f736.jpg");
+
+				HttpURLConnection conn = (HttpURLConnection) url
+						.openConnection();
+				conn.setConnectTimeout(5 * 1000);
+				conn.setReadTimeout(5 * 1000);
+				conn.setRequestMethod("GET");
+				conn.setUseCaches(true);
+				if (conn.getResponseCode() == 200) {
+					InputStream is = conn.getInputStream();
+
+					timecurrentTimeMillis2 = System.currentTimeMillis();
+					time = timecurrentTimeMillis2 - timecurrentTimeMillis;
+					Log.d("yao", time + "");// 请求时间127ms
+					return BitmapFactory.decodeStream(is);
+				} else {
+					Log.d("yao", "server异常...");
 				}
+
+//				timecurrentTimeMillis = System.currentTimeMillis();
+//				HttpGet get = new HttpGet(
+//						"http://g.hiphotos.baidu.com/image/pic/item/060828381f30e9244e3f894a49086e061d95f736.jpg");
+//				HttpClient client = new DefaultHttpClient();
+//				HttpResponse response = client.execute(get);
+//				InputStream is = response.getEntity().getContent();
+//				timecurrentTimeMillis2 = System.currentTimeMillis();
+//				time = timecurrentTimeMillis2 - timecurrentTimeMillis;
+//				Log.d("yao", time + "");// 请求时间183ms
+//				return BitmapFactory.decodeStream(is);
 				
-			} catch (Exception e) {
+				//可知使用HttpUrlConnection请求明显较快与HttpClient
+			} catch (IOException e) {
 				e.printStackTrace();
 			}
-			
-			return result;
+			return null;
 		}
-		
+
 		@Override
-		protected void onPostExecute(String result) {
-			super.onPostExecute(result);
-			if (result!=null) {
-				Log.d("yao", "onPostExecute执行"+result);
+		protected void onPostExecute(Bitmap bitmap) {
+			super.onPostExecute(bitmap);
+			if (bitmap != null) {
+				Log.d("yao", "onPostExecute执行ִ" + bitmap);
+				iv.setImageBitmap(bitmap);
 			}
-			
+
 		}
 
 	}
